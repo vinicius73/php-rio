0f1f4e82be68cdad4e31ab236db4d4c7c90c565b
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-04-21 14:58:29 -0300

    ajutes menores

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,9 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/assets/css/vendors.css">
+        <link rel="stylesheet" href="/assets/css/main.css">
     </head>
     <body>
         @yield('body')
+        <script src="/assets/js/vendors.js" charset="utf-8"></script>
+        <script src="/assets/js/app.bundle.js" charset="utf-8"></script>
     </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,5 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+<h1 class="text-center">PHP Rio 2017! <i class="fa fa-cogs"></i></h1>
 @endsection
